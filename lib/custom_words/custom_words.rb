0420fce8ee4c41388df4a08bb816f735574fa288
Antonio Augusto <antonioabritto@gmail.com>
2015-08-17 11:25:43 -0300

    add nested custom words feature

--- lib/custom_words/custom_words.rb
+++ lib/custom_words/custom_words.rb
@@ -1,11 +1,16 @@
 module CustomWords
-	class CustomWords
+  class CustomWords
 
-		CUSTOM_WORDS = YAML.load_file('config/custom_words.yml')
+    CUSTOM_WORDS = YAML.load_file('config/custom_words.yml')
 
-		def self.fetch_word key
-			CUSTOM_WORDS.fetch(key.to_s)
-		end
+    def self.fetch_word key
+      keys = key.to_s.split('.')
+      result = CUSTOM_WORDS.fetch keys.shift
+      keys.each do |key|
+        result = result.fetch key.to_s
+      end
+      result
+    end
 
-	end
+  end
 end
